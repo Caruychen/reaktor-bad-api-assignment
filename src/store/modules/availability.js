12f1a054c69c416f5b88da5186e7433f07a4b610
Caruychen <caruychen@gmail.com>
2020-11-11 11:00:24 +0200

    tweak module code order

--- src/store/modules/availability.js
+++ src/store/modules/availability.js
@@ -10,7 +10,7 @@ export default {
       return state.availability[manufacturer].items.find(item => {
         return item.id.toLowerCase() === id
       })
-      .DATAPAYLOAD.match(/<INSTOCKVALUE>(.*?)<\/INSTOCKVALUE>/)[1];
+        .DATAPAYLOAD.match(/<INSTOCKVALUE>(.*?)<\/INSTOCKVALUE>/)[1];
     }
   },
   mutations: {
@@ -34,13 +34,13 @@ export default {
   actions: {
     fetchAvailability: ({ state, commit, dispatch }, manufacturer) => {
       const manufacturerData = state.availability[manufacturer];
-      if (manufacturerData) {
+      if (!manufacturerData) {
+        commit("initAvailabilityManufacturer", manufacturer); // Initialize new manufacturer object if not found in state
+      } else {
         if (manufacturerData.isFetching) {
           return manufacturerData.fetchCall; // Return Promise object from state if already fetching
         }
         commit("setAvailabilityIsFetching", { manufacturer, isFetching: true });
-      } else {
-        commit("initAvailabilityManufacturer", manufacturer); // Initialize new manufacturer if not found in state
       }
       const availabilityCall = dispatch("availabilityCall", manufacturer);
       // set the fetch request promise object to the associated manufacturer item
@@ -52,7 +52,7 @@ export default {
     },
     availabilityCall: async ({ state, commit }, manufacturer) => {
       const items = state.availability[manufacturer].items;
-      if (!items || items.length === 0) {
+      if (!items || items.length <= 0) {
         const availability = await badApiHTTP.get("availability/" + manufacturer);
         if (availability.status === 200) {
           commit("setAvailability", {
