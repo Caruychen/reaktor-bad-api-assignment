3e4c702387b19f4580b279f3a9f292274edcc6d2
Caruychen <caruychen@gmail.com>
2020-11-13 16:51:02 +0200

    add error handling

--- src/service/index.js
+++ src/service/index.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import router from "@/router/index.js";
 
 const badApiHTTP = axios.create({
   baseURL: "https://bad-api-assignment.reaktor.com/"
@@ -9,7 +10,6 @@ const badApiHTTP = axios.create({
 badApiHTTP.interceptors.response.use(
   response => {
     const originalConfig = response.config;
-
     // Resend availability requests that returned with an empty array
     if (originalConfig.url.includes("availability")) {
       const availabilityRes = response.data.response
@@ -18,12 +18,27 @@ badApiHTTP.interceptors.response.use(
         return badApiHTTP(originalConfig);
       }
     }
-
     return response;
   },
-  error => {
-    console.log(error)
+  async error => {
+    const errResponse = error.response;
+    const originalConfig = errResponse.config;
+    // Address other potential common errors
+    if (errResponse.status === 400 || errResponse.status === 404) {
+      if (originalConfig.url.includes("products")) { // Show not found page when product returns 400 or 404 error
+        router.push({
+          name: "NotFound"
+        });
+      } else if (originalConfig.url.includes("availability")) {
+        return errResponse // BaseAvailability component will register Data not found
+      }
+    } else if (errResponse.status === 503 || errResponse.status === 504) {
+      return await badApiHTTP(originalConfig) // Retry if temporary unavailability or server timeout
+    } else {
+      return errResponse
+    }
   }
+
 )
 
 export { badApiHTTP };
--- src/store/modules/availability.js
+++ src/store/modules/availability.js
@@ -3,6 +3,7 @@ export default {
   state: {},
   getters: {
     getAvailability: state => (manufacturer, id) => {
+      if (!state[manufacturer].items) return "DATA NOT FOUND"
       return state[manufacturer].items.find(item => {
         return item.id.toLowerCase() === id
       })
