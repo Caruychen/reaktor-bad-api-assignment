8ed977accf26fd63fbe8802a1a38aa0e51d42bca
Caruychen <caruychen@gmail.com>
2020-11-18 11:28:57 +0200

    refactor product filter

--- src/store/modules/products.js
+++ src/store/modules/products.js
@@ -6,27 +6,16 @@ export default {
     accessories: { items: null, isFetching: false, fetchCall: null }
   },
   getters: {
-    getProducts: state => (category, page) => {
-      const minIndex = page ? (page - 1) * 100 : 0;
-      const maxIndex = page ? page * 100 : undefined;
-      return state[category].items.map((item, index) => {
-        return {
-          index: index + 1,
-          name: item.name,
-          id: item.id,
-          manufacturer: item.manufacturer,
-          price: item.price,
-          color: item.color.join(', ')
-        }
-      }).slice(minIndex, maxIndex);
-    },
     getFilteredProducts: (state, getters, rootState, rootGetters) => (category, search) => {
       return state[category].items.filter(product => {
         const filteredProducts = Object.entries(product).every(currentProperty => {
           const searchTerm = search[currentProperty[0]];
-          if (!searchTerm) return true
-          if (currentProperty[1].toString().includes(searchTerm)) return true
-          return false
+          return searchTerm
+            ? currentProperty[1].toString().includes(searchTerm)
+            : true;
+          // if (!searchTerm) return true
+          // if (currentProperty[1].toString().includes(searchTerm)) return true
+          // return false
         })
         const availabilityFilter = search.availability 
           ? rootGetters["availability/getAvailability"](product.manufacturer, product.id).includes(search.availability)
@@ -41,7 +30,7 @@ export default {
       const searchTerm = searchInput.toString();
       return getters.getUniqueSet(category, column)
         .filter(item => {
-          return item.toString().includes(searchTerm)
+          return typeof item !== "number" && item.includes(searchTerm)
         })
         .slice(0, maxOptions)
     }
