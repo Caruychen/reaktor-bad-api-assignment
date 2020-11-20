2f674b845b60c9fc22c84020e5fb2cea7b6ad41e
Caruychen <caruychen@gmail.com>
2020-11-20 14:55:59 +0200

    fix regex to work in safari

--- src/store/modules/availability.js
+++ src/store/modules/availability.js
@@ -6,7 +6,7 @@ export default {
       if (!state[manufacturer].items) return "DATA NOT FOUND";
       return state[manufacturer].items.find(item => {
         return item.id.toLowerCase() === id;
-      }).DATAPAYLOAD.match(/(?<=INSTOCKVALUE>)\w*/)[0];
+      }).DATAPAYLOAD.match(/INSTOCKVALUE>(\w*?)<\/INSTOCKVALUE/)[1];
     },
     getAvailabilitySet: (state, getters, rootState) => (category) => {
       return [... new Set(rootState.products[category].items.map(item => {
