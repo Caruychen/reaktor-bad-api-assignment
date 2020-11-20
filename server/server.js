b9eb9cc0eb4e9bc77423b39ca5e35f11d36036e8
Caruychen <caruychen@gmail.com>
2020-11-20 14:14:29 +0200

    fix server

--- server/server.js
+++ server/server.js
@@ -2,17 +2,14 @@ const express = require("express");
 
 const app = express();
 
-// Handle production
-if (process.env.NODE_ENV === 'production') {
-  // Server static folder
-  app.use(express.static(__dirname + '/public'));
+// Server static folder
+app.use(express.static(__dirname + '/public'));
 
-  // Handle SPA
-  app.get(/.*/, (req, res) => {
-    res.sendFile(path.resolve(__dirname + '/public/index.html'));
-  });
-}
+// Handle SPA
+app.get(/.*/, (req, res) => {
+  res.sendFile(path.resolve(__dirname + '/public/index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
